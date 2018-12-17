d98d307b2d0743aa7571ee3137c34e4494e02740
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-17 15:00:08 +0100

    Exercice 03 non corrigé

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tapez un chiffre a !");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entrez un autre chiffre b !");
+            int b = int.Parse(Console.ReadLine());
+
+            a = a + 33;
+            b++;
+
+            int resultat = a / b;
+            Console.WriteLine("Voici le résultat après avoir rajouté 33 a \"a\" et incrémenté b : " + resultat);
+
+            
+        }
+    }
+}
